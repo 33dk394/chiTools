0a4822e2d4e43003167ae90d91088f580b602f4a
chlee <33dk394chlee@gmail.com>
2024-09-25 03:54:15 +0800

    Feat: Add Timer

---
+++ src/main/scripts/list_dir.py
@@ -0,0 +1,2 @@
+import seedir as sd
+sd.seedir(style='lines', itemlimit=10, depthlimit=10, exclude_folders='.git')
---
+++ src/unittest/python/main_tests.py
@@ -0,0 +1,9 @@
+import unittest
+import main
+
+class MainTest(unittest.TestCase):
+    def test_main(self):
+        main.main()
+
+if __name__ == "__main__":
+    unittest.main()
